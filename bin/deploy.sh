97119028a6a74c98768cae031cd507ed3332b796
Paul Weinstein <pdw@weinstein.org>
2019-08-12 16:48:30 +0000

    Adjusting deploy script to be more selective in webroot dir

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,5 +2,5 @@
 
 # Sync from home/repo to prod
 # Need to run with sudo!
-rsync -a --delete /home/pdw/dev.weinstein.org/www/ /var/www/html;
+rsync -a --delete /home/pdw/dev.weinstein.org/www/index.php /var/www/html/index.php;
 rsync -a --delete /home/pdw/dev.weinstein.org/lib/ /var/www/lib;
