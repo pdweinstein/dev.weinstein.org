a426a275666635c586a376887dac9f30cb05eb74
Paul Weinstein <pdw@leonov.attlocal.net>
2019-11-24 15:03:47 -0800

    Updating simple dielpy script to handle dev and prod environments

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-# Sync from home/repo to prod
-# Need to run with sudo!
-rsync -a --delete /home/pdw/dev.weinstein.org/www/index.php /var/www/html/index.php;
-rsync -a --delete /home/pdw/dev.weinstein.org/www/after-dark/ /var/www/html/after-dark;
-rsync -a --delete /home/pdw/dev.weinstein.org/lib/ /var/www/lib;
+if [[ -z "$1" ]]; then
+
+	1>&2 echo "Deploy env arg missing"
+
+elif [[ $1 == "prod" ]]; then
+
+	# Sync from home/repo to prod
+	rsync -a --delete /home/pdw/dev.weinstein.org/www/index.php /var/www/html/index.php;
+	rsync -a --delete /home/pdw/dev.weinstein.org/www/after-dark/ /var/www/html/after-dark;
+	rsync -a --delete /home/pdw/dev.weinstein.org/lib/ /var/www/lib;
+
+elif [[ $1 == "dev" ]]; then
+
+	# Sync from home/repo to dev
+	rsync -a --delete /Users/pdw/Sites/dev.weinstein.org/www/index.php  /Library/WebServer/Documents/index.php;
+	rsync -a --delete /Users/pdw/Sites/dev.weinstein.org/www/after-dark/  /Library/WebServer/Documents/after-dark;
+	rsync -a --delete /Users/pdw/Sites/dev.weinstein.org/lib/  /Library/WebServer/lib;
+
+fi
